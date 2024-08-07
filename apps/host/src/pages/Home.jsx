d41943bc859667ada33c60faa146fbdfa9adddd5
Raphael Denys <rdenys@gmail.com>
2024-08-07 10:59:20 -0300

    fixed link to remote page 1

--- apps/host/src/pages/Home.jsx
+++ apps/host/src/pages/Home.jsx
@@ -11,7 +11,7 @@ export default function Home() {
 				<Button href="/page2">Page 2</Button>
 				<hr />
 				<h3>Remote 1</h3>
-				<Button href="/remote1-2">Page 1 - remote 1</Button>
+				<Button href="/remote1-1">Page 1 - remote 1</Button>
 				<Button href="/remote1-2">Page 2 - remote 1</Button>
 				<hr />
 				<h3>Remote 2</h3>
